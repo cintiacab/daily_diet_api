b31d4a2f345f4e9c0de01b114a7cf582fe46c464
Cintia Cabral <cintiacabral@hotmail.com>
2025-03-25 16:09:46 -0300

    Including D from CRUD

--- app.py
+++ app.py
@@ -50,6 +50,15 @@ def update_meal(id_meal):
 
     return jsonify({"message":"Meal not found"}), 404
 
+@app.route('/meal/<int:id_meal>', methods=['DELETE'])
+def delete_meal(id_meal):
+   meal = Meal.query.get(id_meal)
+   if meal:
+        db.session.delete(meal)
+        db.session.commit()
+        return jsonify({"message":"Meal succesfully deleted"})
+   
+   return jsonify({"message":"Meal not found"}), 404
 
 
 if __name__ == "__main__":
